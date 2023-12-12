8c806b8c9865d5cffc6a742fc293365df62dcdba
wajihfabernovel <144465236+wajihfabernovel@users.noreply.github.com>
2023-12-12 14:42:11 +0100

    Update combined.py

--- combined.py
+++ combined.py
@@ -235,7 +235,7 @@ def brand_ranking (keywords,DB,your_brand_domain):
                             
                             t = t.with_columns(keyword = pl.lit(Keys),brand_domain = pl.lit(domain), brand_ranking= pl.lit(position))
                             competitors = competitors.vstack(t)  
-                final_compet = final_compet.vstack(competitors.head(10))
+                final_compet = final_compet.vstack(competitors.head(30))
                 competitors = competitors.clear()
         else:
             print(f"Failed to fetch data for keyword: {keyword}. Status Code: {response.status_code}")       
